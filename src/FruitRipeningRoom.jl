6cbc1181aad34c5e384799d20050390793dfe339
Cory Simon <cokes@10-249-165-87.wireless.oregonstate.edu>
2022-04-06 13:44:13 -0700

    setting up gas comps

--- src/FruitRipeningRoom.jl
+++ src/FruitRipeningRoom.jl
@@ -2,7 +2,7 @@ module FruitRipeningRoom
 
 using Distributions
 
-export setup_normal_gas_comp_distn
+export GasCompDistribution, setup_gas_comp_distn
 
 
 #=
@@ -18,9 +18,15 @@ end
 
 # vapor pressure of water
 p_H₂O_vapor = 3.1690 * 0.01 # bar
+viable_labels = ["normal", "CO₂ buildup", "C₂H₄ buildup", "C₂H₄ off", "CO₂ & C₂H₄ buildup"]
 
-function setup_normal_gas_comp_distn(σ_water::Float64)
-    return GasComp(
+function setup_gas_comp_distn(σ_water::Float64, label::String)
+    if ! (label in viable_labels)
+        error(label * "not a viable label")
+    end
+    
+    # set up normal gas comp dis'tn. units are bar.
+    gas_comp_distn = GasCompDistribution(
         # C₂H₄ at 150 ppm
         truncated(
             Normal(150e-6, 20e-6), 
@@ -34,12 +40,20 @@ function setup_normal_gas_comp_distn(σ_water::Float64)
             0.0, Inf
         )
     )
-end
+    
+    # modify if anomalous
+    if label == "C₂H₄ off"
+        gas_comp_distn.f_C₂H₄ = Uniform(0.0e-6, 10.0e-6)
+    elseif label == "C₂H₄ buildup"
+		gas_comp_distn.f_C₂H₄ = Uniform(300e-6, 2000e-6)
+    elseif label == "CO₂ buildup"
+		gas_comp_distn.f_CO₂ = Uniform(7500e-6, 20000e-6)
+    elseif label == "CO₂ & C₂H₄ buildup"
+		gas_comp_distn.f_CO₂ = Uniform(7500e-6, 20000e-6)
+		gas_comp_distn.f_C₂H₄ = Uniform(300e-6, 1000e-6)
+    end
 
-function setup_C₂H₄_off_gas_comp_distn(σ_water::Float64)
-    gcd = setup_normal_gas_comp_distn(σ_water)
-    gcd.f_C₂H₄ = Uniform(0.0e-6, 10.0e-6)
-    return gcd
+    return gas_comp_distn
 end
 
 end
