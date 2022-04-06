b6a0dd6a16149c1711257b97d31b2894d61ee305
Cory Simon <cokes@10-249-165-87.wireless.oregonstate.edu>
2022-04-06 13:58:58 -0700

    create data frame

--- src/FruitRipeningRoom.jl
+++ src/FruitRipeningRoom.jl
@@ -1,9 +1,8 @@
 module FruitRipeningRoom
 
-using Distributions
+using Distributions, DataFrames
 
-export GasCompDistribution, setup_gas_comp_distn
-
+export GasCompDistribution, setup_gas_comp_distn, gen_synthetic_gas_compositions
 
 #=
     DEFINE DISTRIBUTIONS OF GAS COMPOSITIONS IN THE FRUIT RIPENING ROOM
@@ -19,8 +18,9 @@ end
 # vapor pressure of water
 p_H₂O_vapor = 3.1690 * 0.01 # bar
 viable_labels = ["normal", "CO₂ buildup", "C₂H₄ buildup", "C₂H₄ off", "CO₂ & C₂H₄ buildup"]
+gases = ["C₂H₄", "CO₂", "H₂O"]
 
-function setup_gas_comp_distn(σ_water::Float64, label::String)
+function setup_gas_comp_distn(σ_H₂O::Float64, label::String)
     if ! (label in viable_labels)
         error(label * "not a viable label")
     end
@@ -36,7 +36,7 @@ function setup_gas_comp_distn(σ_water::Float64, label::String)
         Uniform(400.0e-6, 5000.0e-6),
         # H₂O: 85% RH on average
         truncated(
-            Normal(0.85 * p_H₂O_vapor, σ_water * p_H₂O_vapor), 
+            Normal(0.85 * p_H₂O_vapor, σ_H₂O * p_H₂O_vapor), 
             0.0, Inf
         )
     )
@@ -54,6 +54,22 @@ function setup_gas_comp_distn(σ_water::Float64, label::String)
     end
 
     return gas_comp_distn
+end
+
+function gen_synthetic_gas_compositions(label::String, n_compositions::Int, σ_H₂O::Float64)
+    gas_comp_distn = setup_gas_comp_distn(σ_H₂O, label)
+    data = DataFrame("C₂H₄ [bar]" => zeros(n_compositions), 
+                     "CO₂ [bar]" => zeros(n_compositions),
+                     "H₂O [bar]" => zeros(n_compositions),
+                     "label" => [label for _ = 1:n_compositions]
+                    )
+    for i = 1:n_compositions
+        data[i, "C₂H₄ [bar]"] = rand(gas_comp_distn.f_C₂H₄)
+        data[i, "CO₂ [bar]"]  = rand(gas_comp_distn.f_CO₂)
+        data[i, "H₂O [bar]"]  = rand(gas_comp_distn.f_H₂O)
+        data[i, "label"]      = label
+    end
+    return data
 end
 
 end
